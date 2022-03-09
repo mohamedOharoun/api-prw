0f1b2dafe4c4fcdeddfddda1198f074ca6c4fe04
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2022-03-09 19:20:52 +0000

    Borrado de error

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,14 @@
 const Product = require('../models/product.js');
 const {StatusCodes} = require('http-status-codes');
 
-//Conseguir todos las notas del usuario 
+//Conseguir todos los productos de la empresa 
 const getAllProducts = async(req, res) => {
     const products = await Product.find({company: req.company.companyId}).sort('createdAt');//Se ordenarán situando los más viejos primero
 
     res.status(StatusCodes.OK).json({count: products.length, products});
 }
 
-//Crear nota
+//Crear producto
 const createProduct = async (req, res) => {
     req.body.company = req.company.companyId;//Añadir la id de usuario al conjunto de datos recibidos
 
@@ -17,7 +17,7 @@ const createProduct = async (req, res) => {
     res.status(StatusCodes.CREATED).json({product});
 };
 
-//Actualizar nota
+//Actualizar producto
 const updateProduct = async(req, res) => {
     //Se obtienen las variables del request
     const {
@@ -26,12 +26,12 @@ const updateProduct = async(req, res) => {
         params: {id: productId}
     } = req;
 
-    //Se comprueba que la nota tiene un nombre y contenido
+    //Se comprueba que la producto tiene un nombre y contenido
     if(name === '' || content === ''){
         throw new BadRequestError('Name or Content fields cannot be empty');
     }
 
-    //Se actualiza según la id de la nota y del usuario
+    //Se actualiza según la id de la producto y de la empresa
     const product = await Product.findByIdAndUpdate(
         {
             _id: productId,
@@ -44,14 +44,14 @@ const updateProduct = async(req, res) => {
         }
     );
 
-    if(!product){//En caso de que no exista una nota con tal id
+    if(!product){//En caso de que no exista una producto con tal id
         throw new NotFoundError(`No Product with ${productId}`);
     }
 
     res.status(StatusCodes.OK).json({product});
 }
 
-//Borado de la nota según la id de la nota y el usuario.
+//Borado del producto según la id del producto y el usuario.
 const deleteProduct = async(req, res) => {
     const {
         company: {companyId},
